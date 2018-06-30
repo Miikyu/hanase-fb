a8ba53275b5b492be68d53765a36e17670c520b5
Miikyu <40524609+Miikyu@users.noreply.github.com>
2018-06-30 19:41:05 +0700

    .env

--- commands/giphy.js
+++ commands/giphy.js
@@ -5,7 +5,7 @@ module.exports = (bot) => {
   bot.hear(/gif (.*)/i, (payload, chat, data) => {
   const query = data.match[1];
   chat.say('P-please.. let me pick some perfect Gif for you..');
-  fetch(GIPHY_URL + query)
+  fetch(process.env.GIPHY_URL + query)
     .then(res => res.json())
     .then(json => {
       chat.say({
