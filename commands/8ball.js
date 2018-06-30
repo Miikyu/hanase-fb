3671dff75e84ba96d96eafb003e4b4d3045360eb
Miikyu <40524609+Miikyu@users.noreply.github.com>
2018-06-30 23:38:13 +0700

    Logging

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,29 +1,10 @@
 'use strict';
 
-module.exports = (bot) => {
+module.exports = (bot, chalk) => {
   bot.hear(/hey~ (.*)/i, (payload, chat) => {
-    var answers = ["It is certain",
-										"It is decidedly so",
-										"Without a doubt",
-										"Yes, definitely",
-										"You may rely on it",
-										"As I see it, yes",
-										"Most likely",
-										"Outlook good",
-										"Yes",
-										"Signs point to yes",
-										"Reply hazy try again",
-										"Ask again later",
-										"Better not tell you now",
-										"Cannot predict now",
-										"Concentrate and ask again",
-										"Don't count on it",
-										"My reply is no",
-										"My sources say no",
-										"Outlook not so good",
-										"Very doubtful"
-                  ];
+    var answers = ["It is certain","It is decidedly so","Without a doubt","Yes, definitely","You may rely on it","As I see it, yes","Most likely","Outlook good","Yes","Signs point to yes","Reply hazy try again","Ask again later","Better not tell you now","Cannot predict now","Concentrate and ask again","Don't count on it","My reply is no","My sources say no","Outlook not so good","Very doubtful"];
     var result = Math.floor((Math.random() * answers.length) + 0);
     chat.say(answers[result], {typing: true});
+    console.log(chalk.yellow('[LOG] ') + chalk.white('Successfully giving answer to users'));
   });
 };
