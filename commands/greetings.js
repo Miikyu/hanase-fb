cf8a57f3327853b31187f1a2dd9e03788cd33849
Miikyu <40524609+Miikyu@users.noreply.github.com>
2018-06-30 23:02:59 +0700

    Fix Conflict Greetings with 8ball.

--- commands/greetings.js
+++ commands/greetings.js
@@ -1,7 +1,7 @@
 
 'use strict';
 module.exports = (bot) => {
-    bot.hear(['hello', 'hi', 'halo' , 'oy', 'hai', /hey( there)?/i], (payload, chat) => {
+    bot.hear(['hello'], (payload, chat) => {
         chat.getUserProfile().then((user) => {
             chat.say('Hello ' + user.first_name + '-san.., nice to meet you..').then(() => {
                 chat.say('You can use "help" to get m-my commands list.., or use Hanase Menu', { typing: true });
