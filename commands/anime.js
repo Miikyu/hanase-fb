f787590eee8e74676c3c462d67c3b23f480c8100
Miikyu <40524609+Miikyu@users.noreply.github.com>
2018-07-01 08:39:46 +0700

    Update anime.js

--- commands/anime.js
+++ commands/anime.js
@@ -8,7 +8,7 @@ module.exports = (bot) => {
    	chat.say('L-let me pull some information about this anime...');
 		kitsu.searchAnime(querys, 0).then(results => {
 			chat.say('Check Log');
-    	console.log(results[id]);
+    	console.log(results[1]);
 	});
   });
 };
