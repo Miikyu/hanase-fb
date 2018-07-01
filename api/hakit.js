8e656c9587ba9c46beccd13f1ff2cfb530bb87fa
Miikyu <40524609+Miikyu@users.noreply.github.com>
2018-07-01 09:44:36 +0700

    Custom Kitsu API based from node-kitsu

--- api/hakit.js
+++ api/hakit.js
@@ -1,1 +1,25 @@
+const request = require('request');
 
+// Kitsu API
+// Original Source from the-conceptionist/node-kitsu
+
+exports.searchAnime = function(query, offset) {
+    return new Promise(function(resolve, reject) {
+        request({
+            method: 'GET',
+            url: 'https://kitsu.io/api/edge/anime?filter[text]=' + query + '&page%5Boffset%5D=' + (offset.toString() ? offset : '0'),
+            headers: {
+                'Content-Type': 'application/vnd.api+json',
+                'Accept': 'application/vnd.api+json'
+            }
+        }, function(error, response, body) {
+            if (error) {
+                reject(Error(error))
+            } else {
+                var allofit = JSON.parse(body)
+                var results = allofit.data
+                resolve(results)
+            }
+        })
+    })
+}
