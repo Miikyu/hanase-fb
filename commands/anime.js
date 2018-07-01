c171623d2fb50edd088bbd4ca391983e05037752
Miikyu <40524609+Miikyu@users.noreply.github.com>
2018-07-01 10:01:42 +0700

    Fix Variable

--- commands/anime.js
+++ commands/anime.js
@@ -4,7 +4,7 @@ const hakit = require('../api/hakit');
 
 module.exports = (bot) => {
   bot.hear(/anifo (.*)/i, (payload, chat, data) => {
-    query = data.match[1];
+    var query = data.match[1];
     chat.say(`Okay!, l-let me searching for ${query}`);
     hakit.animeFindTitleEN(query, 0)
       .then(result => {
