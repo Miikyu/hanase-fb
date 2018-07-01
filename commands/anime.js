34444c6f750b6956cef4f07de5d4b98646055b3d
Miikyu <40524609+Miikyu@users.noreply.github.com>
2018-07-01 09:56:48 +0700

    Anime Command using custom Kitsu API

---
+++ commands/anime.js
@@ -0,0 +1,15 @@
+
+'use strict';
+const hakit = require('../api/hakit');
+
+module.exports = (bot) => {
+  bot.hear(/anifo (.*)/i, (payload, chat, data) => {
+    query = data.match[1];
+    chat.say(`Okay!, l-let me searching for ${query}`);
+    hakit.animeFindTitleEN(query, 0)
+      .then(result => {
+        chat.say('Check Log!');
+        console.log(result[0]);
+      });
+  };
+};
